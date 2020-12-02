00ec9b32faf666fabf71cdc3996ceca4b8d0897e
jashan <jashan@gmail.com>
2020-12-02 22:30:36 +0530

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -39,30 +39,7 @@ export class HomePage implements OnInit {
 	  
 	  
   	ngOnInit(){
-  		// Optionally request the permission early
-		this.qrScanner.prepare()
-		  .then((status: QRScannerStatus) => {
-		     if (status.authorized) {
-		       // camera permission was granted
-
-
-		       // start scanning
-		       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-		         console.log('Scanned something', text);
-
-		         this.qrScanner.hide(); // hide camera preview
-		         scanSub.unsubscribe(); // stop scanning
-		       });
-
-		     } else if (status.denied) {
-		       // camera permission was permanently denied
-		       // you must use QRScanner.openSettings() method to guide the user to the settings page
-		       // then they can grant the permission from there
-		     } else {
-		       // permission was denied, but not permanently. You can ask for permission again at a later time.
-		     }
-		  })
-		  .catch((e: any) => console.log('Error is', e));
+  		
   	}
   	
   	ionViewWillEnter(){
@@ -72,6 +49,42 @@ export class HomePage implements OnInit {
 	        this.usersProvider.GetUserByToken(token).subscribe(data => {
 	          this.user = data.details;
 	          if(this.user){
+
+	          	// Optionally request the permission early
+				this.qrScanner.prepare()
+				  .then((status: QRScannerStatus) => {
+				     if (status.authorized) {
+				       // camera permission was granted
+
+
+				       // start scanning
+				       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+				         console.log('Scanned something', text);
+
+				         this.qrScanner.hide(); // hide camera preview
+				         scanSub.unsubscribe(); // stop scanning
+				       });
+
+				       this.qrScanner.resumePreview();
+
+          // show camera preview
+          this.qrScanner.show();
+
+
+				     } else if (status.denied) {
+				     	alert('denied');
+				       // camera permission was permanently denied
+				       // you must use QRScanner.openSettings() method to guide the user to the settings page
+				       // then they can grant the permission from there
+				     } else {
+				       // permission was denied, but not permanently. You can ask for permission again at a later time.
+				       alert('else');
+				     }
+				  })
+				  .catch((e: any) => console.log('Error is', e));
+
+
+
 	            this.restaurantProvider.GetAllRestaurants().subscribe(data => {
 			 
 					this.shops = [];
